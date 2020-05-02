dbbf479de19b1966a8baedc526a035af5d0e6cd6
G Bilge Dal <lagala999@gmail.com>
2020-05-02 08:14:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -77,3 +77,11 @@ function findEmployeeByFirstName(srcArray, firstName){
     }
   }
 }
+function calculatePayroll(srcArray){
+  let sum = 0;
+  for(let i = 0; i < srcArray.length; i++){
+    sum += allWagesFor(srcArray[i]);
+  }
+  return sum
+  
+}
