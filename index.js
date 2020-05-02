262a0e3978609a2638d798c8b06dbc921e191e67
G Bilge Dal <lagala999@gmail.com>
2020-05-02 07:57:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Your code here
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+        firstName: firstName
+        , familyName: familyName
+        , title: title
+        , payPerHour: payPerHour
+        , timeInEvents: []
+        , timeOutEvents: []
+    }
+}
+
+
+
+function createEmployeeRecords(employees){
+  let emplo = [];
+  for(let i = 0;i<employees.length;i++){
+    emplo.push(createEmployeeRecord(employees[i]));
+  }
+  return emplo;
+}
+
+function createTimeInEvent(employee, date){
+
+
+}
